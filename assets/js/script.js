b480d3bd1e469d14e440b7c285b7730c2ba3262e
ElisabethKAndersson <>
2023-06-05 23:27:47 +0000

    Add some questions to js

---
+++ assets/js/script.js
@@ -0,0 +1,22 @@
+// Questions
+
+let questions = [
+    {
+        id: "0"
+        question: `Which is the largest country in the world?`,
+        answers: ['Canada', 'China', 'Russia'],
+        correctAnswer: 'Russia',
+    }
+    {
+        id: "1"
+        question: `What continent has the most countries?`,
+        answers: ['Africa', 'Europe', 'Asia'],
+        correctAnswer: 'Africa',
+    }
+    {
+        id: "2"
+        question: `What is the capital of Australia`,
+        answers: ['Melbourne', 'Sydney', 'Canberra'],
+        correctAnswer: 'Canberra',
+    }
+];
