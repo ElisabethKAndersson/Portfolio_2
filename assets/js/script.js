785e26a4ee88a1bbeda77114d2b3b504f75f5627
ElisabethKAndersson <>
2023-06-06 23:05:26 +0000

    add reset function

--- assets/js/script.js
+++ assets/js/script.js
@@ -24,12 +24,14 @@ const questions = [
             { text: 'Sidney', correct: false },
             { text: 'Canberra', correct: true },
         ]
-    };
+    }
+
+];
 
 //Get elements from the DOM
 
 const questionElement = document.getElementById('question');
-const answerButton = document.getElementById('answer-buttons');
+const answerButtons = document.getElementById('answer-buttons');
 const nextButton = document.getElementById('next-button');
 
 let currentQuestionIndex = 0;
@@ -44,6 +46,7 @@ function runGame() {
 }
 
 function showQuestion() {
+    resetState();
     let currentQuestion = questions[currentQuestionIndex];
     let questionNo = currentQuestionIndex + 1;
     questionElement.innerHTML = questionNo + ". " + currentQuestion.question;
@@ -52,8 +55,15 @@ function showQuestion() {
         const button = document.createElement("button");
         button.innerHTML = answer.text;
         button.classList.add("btn");
-        answerButton.appendChild(button);
+        answerButtons.appendChild(button);
     });
+}
+
+function resetState() {
+    nextButton.style.display = "none";
+    while (answerButtons.firstChild) {
+        answerButtons.removeChild(answerButtons.firstChild);
+    }
 }
 
 runGame();
