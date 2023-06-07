f836d43c5b8ce8d99a262ae17b7ed0d58a263781
ElisabethKAndersson <>
2023-06-07 23:25:05 +0000

    Remove new code test and go back to old project

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,20 @@
 /*This quiz has been made with support of following tutorial: 
 https://www.codingninjas.com/codestudio/library/how-to-create-a-javascript-quiz-code */
 
-const allQuestions = [
+
+//Get Elements from DOM
+const nextBtn = getElementById = ('next-button');
+const submitBtn = getElementsById = ('submit-button');
+const restartBtn = getElementById = ('restart-button');
+const trueBtn = getElementById = ('true');
+const falseBtn = getElementById = ('false');
+const userScore = getElementById = ('user-score');
+const questionText = getElementById = ('question-text');
+
+let currentQuestion = 0;
+let score = 0;
+
+const questions = [
     {
         question: `Russia is the largest country in the world.`,
         answers: [
@@ -22,28 +35,56 @@ const allQuestions = [
             { option: 'TRUE', answer: false },
             { option: 'FALSE', answer: true }
         ]
-    },
-
-    {
-        question: `What is the capital of Australia?`,
-        answers: {
-            a: 'Melbourne',
-            b: 'Sidney',
-            c: 'Canberra';
-        },
-        correctAnswer: 'c';
     }
-];
+]
 
-//Get Elements from DOM
-const nextBtn = getElementById = ('next-button');
-const submitBtn = getElementsById = ('submit-button');
-const restartBtn = getElementById = ('restart-button');
-const trueBtn = getElementById = ('true');
-const falseBtn = getElementById = ('false');
-const userScore = getElementById = ('user - score');
-const questionText = getElementById = ('question');
+// EventListerners
 
-let currentQuestion = 0;
-let score = 0;
+restartBtn.addEventListener('click', restart);
+nextBtn.addEventListener('click', next);
+submitBtn.addEventListener('click', submit);
 
+//Game starter with true/false button parameters
+function runGame() {
+    currentQuestion = 0;
+    questionText.innerHTML = questions[currentQuestion].question;
+    trueBtn.innerHTML = questions[currentQuestion].answers[0].option;
+    trueBtn.onclick = () => {
+        let ano = 0;
+        if (questions[currentQuestion].answers[ano].answer) {
+            if (score < 3) {
+                score++;
+            }
+        }
+        userScore.innerHTML = score;
+        if (currentQuestion < 2) {
+            next();
+        }
+    };
+    falseBtn.innerHTML = questions[currentQuestion].answers[1].option;
+    falseBtn.onclick = () => {
+        let ano = 1;
+        if (questions[currentQuestion].answers[ano].answer) {
+            if (score < 3) {
+                score++;
+            }
+        }
+        userScore.innerHTML = score;
+        if (currentQuestion < 2) {
+            next();
+        }
+    }
+}
+runGame();
+
+//reset at gamestart
+function restart() {
+    currentQuestion = 0;
+    nextBtn.classList.remove('hide');
+    submitBtn.classList.remove('hide');
+    trueBtn.classList.remove('hide');
+    falseBtn.classList.remove('hide');
+    score = 0;
+    userScore.innerHTML = score;
+    runGame();
+}
